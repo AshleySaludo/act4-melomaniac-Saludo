50e8ebb57d1fbbb1694f2a02af24ec592d6bbf29
SaludoAshley <ashleycanoy13@gmail.com>
2022-04-14 22:22:05 +0800

    track5

---
+++ Song4.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="https://localhost/melomaniac-activity/index.php"> Making Music Stories </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Making Music Stories</h1>
+                <p class="lead">Life and love go on, let the music play</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        <?php 
+
+                        echo "<h2 class='text-primary'>Nobela</h2>";
+                        echo "<p class='lead'>Join The Club</p>";
+
+                        echo "<p> Ngumiti kahit na napipilitan</p>";
+                        echo "<p> Kahit pa sinasadya</p>";
+                        echo "<p> Mo akong masaktan paminsan-minsan</p>";
+                        echo "<p> Bawat sandali na lang</p>";
+                        echo "<p> Tulad mo ba akong nahihirapan</p>";
+                        echo "<p> Lalo't naiisip ka</p>";
+                        echo "<p> Di ko na kaya pa na kalimutan</p>";
+                        echo "<p> Bawat sandali na lang</p>";
+                        echo "<br/>";
+
+                        echo "<p> At aalis magbabalik</p>";
+                        echo "<p> At uuliting sabihin</p>";
+                        echo "<p> Na mahalin ka't sambitin</p>";
+                        echo "<p> Kahit muling masaktan</p>";
+                        echo "<p> Sa pag-alis</p>";
+                        echo "<p> Ako'y magbabalik</p>";
+                        echo "<p> At sana naman</p>";
+                        echo "<br?>";
+
+                        echo "<p> Sa isang marikit na alaala'y</p>";
+                        echo "<p> Pangitaing kay ganda</p>";
+                        echo "<p> Sana nga'y pagbigyan</p>";
+                        echo "<p> Na ng tadhana</p>";
+                        echo "<p> Bawat sandali na lang</p>";
+                        echo "<p> Sumabay sa biglang pagkabahala't</p>";
+                        echo "<p> Lumabis ang pagtataka</p>";
+                        echo "<p> Tunay na pagsintang di alintana</p>";
+                        echo "<p> Bawat sandali na lang</p>";
+                        echo "<br/>";
+
+                        echo "<p> At aalis magbabalik</p>";
+                        echo "<p> At uuliting sabihin</p>";
+                        echo "<p> Na mahalin ka't sambitin</p>";
+                        echo "<p> Kahit muling masaktan</p>";
+                        echo "<p> Sa pag-alis</p>";
+                        echo "<p> Ako'y magbabalik</p>";
+                        echo "<p> At sana naman</p>";
+                        echo "<br?>";
+
+                        echo "<p> Ngumiti kahit na napipilitan</p>";
+                        echo "<p> Kahit pa sinasadya</p>";
+                        echo "<p> Mo akong masaktan paminsan-minsan</p>";
+                        echo "<p> Bawat sandali na lang</p>";
+                        echo "<br?>";
+
+                        echo "<p> At aalis magbabalik</p>";
+                        echo "<p> At uuliting sabihin</p>";
+                        echo "<p> Na mahalin ka't sambitin</p>";
+                        echo "<p> Kahit muling masaktan</p>";
+                        echo "<p> Sa pag-alis</p>";
+                        echo "<p> Ako'y magbabalik</p>";
+                        echo "<p> At sana naman</p>";
+                        echo "<br?>";
+
+                        
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Ashley C. Saludo</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
+
