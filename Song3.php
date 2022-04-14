6cd5bcefc1d36f2f01344477f7d96c673a296cbc
SaludoAshley <ashleycanoy13@gmail.com>
2022-04-14 22:13:21 +0800

    track4

---
+++ Song3.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="https://localhost/melomaniac-activity/index.php"> Making Music Stories </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Making Music Stories</h1>
+                <p class="lead">Life and love go on, let the music play</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        <?php 
+
+                        echo "<h2 class='text-primary'>Paano Ba Ang Magmahal</h2>";
+                        echo "<p class='lead'>Piolo Pascual & Sarah Geronimo</p>";
+
+                        echo "<p> Heto na naman ako</p>";
+                        echo "<p> Nag-aabang ng bago sa istorya ko</p>";
+                        echo "<p> Paulit-ulit na lang</p>";
+                        echo "<p> Paulit-ulit na lang</p>";
+                        echo "<br/>";
+
+                        echo "<p> Heto na naman ako</p>";
+                        echo "<p> Tinitignan sa'n nagkamali ang puso ko</p>";
+                        echo "<p> Parang walang katapusan</p>";
+                        echo "<p> Walang katapusan</p>";
+                        echo "<br?>";
+
+                        echo "<p> Kahit pilitin pa'ng sarili</p>";
+                        echo "<p> Ibigin ka, mali</p>";
+                        echo "<p> Ako'y mali</p>";
+                        echo "<p> Ako'y mali</p>";
+                        echo "<br/>";
+
+                        echo "<p> Paano ba ang magmahal</p>";
+                        echo "<p> Palagi bang nasasaktan</p>";
+                        echo "<p> Umiiyak na lang palagi</p>";
+                        echo "<p> Gusto ko nang lumisan</p>";
+                        echo "<p> Paano ba ang magmahal</p>";
+                        echo "<p> Kailangan bang nasasaktan</p>";
+                        echo "<p> Lagi na lang, di maaari</p>";
+                        echo "<p> Ngunit ayaw lumisan</p>";
+                        echo "<br?>";
+
+                        echo "<p> Heto na naman ako</p>";
+                        echo "<p> Parang hindi nadadala ang puso ko</p>";
+                        echo "<p> Kahit nasusugatan</p>";
+                        echo "<p> Aking ipaglalaban</p>";
+                        echo "<br?>";
+
+                        echo "<p> Kahit pilitin pa'ng sarili</p>";
+                        echo "<p> Ibigin ka, mali</p>";
+                        echo "<p> Parang mali</p>";
+                        echo "<p> Parang mali</p>";
+                        echo "<br?>";
+
+                        echo "<p> Paano ba ang magmahal</p>";
+                        echo "<p> Palagi bang nasasaktan</p>";
+                        echo "<p> Umiiyak na lang palagi</p>";
+                        echo "<p> Gusto ko nang lumisan</p>";
+                        echo "<p> Paano ba ang magmahal</p>";
+                        echo "<p> Kailangan bang nasasaktan</p>";
+                        echo "<p> Lagi na lang, di maaari</p>";
+                        echo "<p> Ngunit ayaw lumisan</p>";
+                        echo "<br?>";
+
+                        echo "<p> Kailan ba, ang tamang panahon</p>";
+                        echo "<p> Kailan ba, magkakataong</p>";
+                        echo "<p> Malaya na ang puso mo</p>";
+                        echo "<p> At puso ko</p>"; 
+
+                        echo "<p> Paano ba ang magmahal</p>";
+                        echo "<p> Palagi bang nasasaktan</p>";
+                        echo "<p> Umiiyak na lang palagi</p>";
+                        echo "<p> Gusto ko nang lumisan</p>";
+                        echo "<p> Paano ba ang magmahal</p>";
+                        echo "<p> Kailangan bang nasasaktan</p>";
+                        echo "<p> Lagi na lang, di maaari</p>";
+                        echo "<p> Ngunit ayaw lumisan</p>";
+                        echo "<br?>";
+
+                        echo "<p> Paano ba ang magmahal</p>";
+                        echo "<p> Palagi bang nasasaktan</p>";
+                        echo "<p> Umiiyak na lang palagi</p>";
+                        echo "<p> Gusto ko nang lumisan</p>";
+                        echo "<p> Paano ba ang magmahal</p>";
+                        echo "<p> Kailangan bang nasasaktan</p>";
+                        echo "<p> Umiiyak na lang palagi</p>";
+                        echo "<p> Lagi na lang, di maaari</p>";
+                        echo "<p> Umiiyak na lang palagi</p>";
+                        echo "<p> Lagi na lang, di maaari</p>";
+                        echo "<p> Ngunit ayaw lumisan</p>";
+                        echo "<br?>";
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Ashley C. Saludo</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
+
