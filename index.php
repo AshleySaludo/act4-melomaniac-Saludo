e80b979a847b04d7862a8e10686c82d4672685a5
SaludoAshley <ashleycanoy13@gmail.com>
2022-04-14 21:46:35 +0800

    track1

--- index.php
+++ index.php
@@ -14,15 +14,15 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
             <div class="container px-4">
-                <a class="navbar-brand" href="#page-top"> Add your brand name here </a>
+                <a class="navbar-brand" href="https://localhost/melomaniac-activity/index.php"> Making Music Stories </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 1</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 2</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 3</a></li>
-                         <li class="nav-item"><a class="nav-link text-light" href="#">Song 4</a></li>
-                          <li class="nav-item"><a class="nav-link text-light" href="#">Song 5</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song5.php">Song 5</a></li>
                     </ul>
                 </div>
             </div>
@@ -30,8 +30,8 @@
         <!-- Header-->
         <header class="bg-dark bg-gradient text-white">
             <div class="container px-4 text-center">
-                <h1 class="fw-bolder">Welcome to Melomaniac</h1>
-                <p class="lead">Add a short description here</p>
+                <h1 class="fw-bolder">Making Music Stories</h1>
+                <p class="lead">Life and love go on, let the music play</p>
                 
             </div>
         </header>
@@ -40,16 +40,51 @@
             <div class="container px-4 ">
                 <div class="row gx-4 justify-content-center">
                     <div class="col-lg-12 text-center">
-                        <h2 class="text-primary">Song Title here</h2>
-                        <p class="lead">display the Composer here</p>
-                        <p>display the lyrics/chords here</p>
+
+                        <?php 
+
+                        echo "<h2 class='text-primary'>Ulan</h2>";
+                        echo "<p class='lead'>Cueshe</p>";
+
+                        echo "<p> Lagi nalang umuulan</p>";
+                        echo "<p> Parang walang katapusan</p>";
+                        echo "<p> Tulad ng paghihirap ko ngayon</p>";
+                        echo "<p> Parang walang humpay</p>";
+                        echo "<p> Sa kabila ng lahat ng aking pagsisikap</p>";
+                        echo "<p> Na limutin ka ay di pa rin magawa</p>";
+                        echo "<br/>";
+
+                        echo "<p> Hindi naman ako tanga</p>";
+                        echo "<p> Alam ko nang wala ka na</p>";
+                        echo "<p> Pero mahirap lang na tanggapin</p>";
+                        echo "<p> Di na kita kapiling</p>";
+                        echo "<br?>";
+
+                        echo "<p> Iniwan mo akong nagiisa</p>";
+                        echo "<p> Sa gitna ng dilim at basang-basa pa sa ulan</p>";
+                        echo "<br/>";
+
+                        echo "<p> Pero wag mag-alala di na kita gagambalain pa</p>";
+                        echo "<p> Alam ko namang ngayon may kapiling ka nang iba</p>";
+                        echo "<br/>";
+
+                        echo "<p> Tanging hiling ko sayo</p>";
+                        echo "<p> Na tuwing umuulan</p>";
+                        echo "<p> Maalala mo sanang may</p>";
+                        echo "<p> Nagmamahal sayo-ako</p>";
+                        echo "<p> Lalalalalalalalala...</p>";
+                        echo "<br/>";
+
+                        ?>
+
+
                     </div>
                 </div>
             </div>
         </section>
 
         <footer class="py-3 bg-danger">
-            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. add your fullname here</p></div>
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Ashley C. Saludo</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="js/bootstrap.bundle.min.js"></script>
@@ -57,3 +92,4 @@
         <script src="js/scripts.js"></script>
     </body>
 </html>
+
