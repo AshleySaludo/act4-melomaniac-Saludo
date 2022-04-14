ef7362a785ce1c31d20b1b13e825c8ea10f0b00b
SaludoAshley <ashleycanoy13@gmail.com>
2022-04-14 22:32:02 +0800

    track6

---
+++ Song5.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="https://localhost/melomaniac-activity/index.php"> Making Music Stories </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Making Music Stories</h1>
+                <p class="lead">Life and love go on, let the music play</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        <?php 
+
+                        echo "<h2 class='text-primary'>Buwan</h2>";
+                        echo "<p class='lead'>Juan Karlos Labajo</p>";
+
+                        echo "<p> Ako'y sayo ikaw ay akin</p>";
+                        echo "<p> Ganda mo sa paningin</p>";
+                        echo "<p> Ako ngayo'y nag-iisa</p>";
+                        echo "<p> Sana ay tabihan na</p>";
+                        echo "<br/>";
+
+                        echo "<p> Sa ilalim ng puting ilaw</p>";
+                        echo "<p> Sa dilaw na buwan</p>";
+                        echo "<p> Pakinggan mo ang aking sigaw</p>";
+                        echo "<p> Sa dilaw na buwan</p>";
+                        echo "<br?>";
+
+                        echo "<p> Ayokong mabuhay ng malungkot</p>";
+                        echo "<p> Ikaw ang nagpapasaya</p>";
+                        echo "<p> At makakasama hanggang sa pagtanda</p>";
+                        echo "<p> Halina't tayo'y humiga</p>";
+                        echo "<br/>";
+
+                        echo "<p> Sa'n kaya?</p>";
+                        echo "<br?>";
+
+                        echo "<p> Sa ilalim ng puting ilaw</p>";
+                        echo "<p> Sa dilaw na buwan</p>";
+                        echo "<p> Pakinggan mo ang aking sigaw</p>";
+                        echo "<p> Sa dilaw na buwan</p>";
+                        echo "<br?>";
+
+                        echo "<p> Ang iyong ganda'y umaabot sa buwan</p>";
+                        echo "<p> Ang tibok ng puso'y rinig sa kalawakan</p>";
+                        echo "<p> At bumabalik</p>";
+                        echo "<p> Dito sa akin</p>";
+                        echo "<p> Ikaw ang mahal</p>";
+                        echo "<p> Ikaw lang ang mamahalin</p>";
+                        echo "<p> Pakinggan ang puso't damdamin</p>";
+                        echo "<p> Damdamin aking damdamin</p>";
+                        echo "<br?>";
+
+                        echo "<p> Sa ilalim ng puting ilaw</p>";
+                        echo "<p> Sa dilaw na buwan</p>";
+                        echo "<p> Pakinggan mo ang aking sigaw</p>";
+                        echo "<p> Sa dilaw na buwan</p>";
+                        echo "<br?>";
+
+                        echo "<p> Sa ilalim ng puting ilaw</p>";
+                        echo "<p> Sa dilaw na buwan</p>";
+                        echo "<p> Pakinggan mo ang aking sigaw</p>";
+                        echo "<p> Sa dilaw na buwan</p>";
+                        echo "<br?>";
+
+                        
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Ashley C. Saludo</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
+
